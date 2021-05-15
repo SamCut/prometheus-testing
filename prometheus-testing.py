47362101c7d79e73e997c75768df1174bf83e01a
SA <samacut@outlook.com>
2021-05-15 13:44:47 -0700

    undo

--- prometheus-testing.py
+++ prometheus-testing.py
@@ -1,8 +1,7 @@
 from prometheus_client import start_http_server, Summary
 import random
 import time
-import bussy
-#TEST
+
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
